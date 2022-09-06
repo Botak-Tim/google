affd1634a2a6c685b440a5313ebc9aba1b1f18a5
Alandrian S.T <111427255+LanLan1412@users.noreply.github.com>
2022-09-06 13:07:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('selamat datang..');
+var lagi = true;
+
+while(lagi === true) {
+  var nama = prompt('masukkan nama:');
+  alert('haloo ' + nama);
+
+  lagi = confitm('mau coba lagi?');
+}
+
+alert('terimakasih...');
